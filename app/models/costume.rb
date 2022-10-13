2847bfa84d84fa796042dceb578d03fd73ca22b2
GarlicGrapes <austinlongocontracting@gmail.com>
2022-10-12 23:23:10 -0400

    commit tree b2c6d2cf5293147651fc6706ec64dfaf8c8ea664

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,18 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+# class CreateCostumes < ActiveRecord::Migration[6.1]
+#     def change
+#         create_table :costumes do |t|
+#             t.string :name
+#             t.float :price
+#             t.string :size
+#             t.string :image_url
+#             t.datetime :created_at
+#             t.datetime :updated_at
+#         end
+#     end
+# end
+
+class Costume < ActiveRecord::Base
+
+end
--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,24 @@
 # Create your CostumeStore class here
+# class CreateX < ActiveRecord::Migration[6.1]
+#     def change
+#         create_table :x do |t|
+#             t.datatype :title
+#         end
+#     end
+# end
+
+# class CreateCostumeStores < ActiveRecord::Migration[6.1]
+#     def change
+#         create_table :costume_stores do |t|
+#             t.string :name
+#             t.string :location
+#             t.integer :costume_inventory
+#             t.integer :number_of_employees
+#             t.boolean :is_in_business
+#             t.datetime :opening_time
+#             t.datetime :closing_time
+#         end
+#     end
+# end
+class CostumeStore < ActiveRecord::Base
+end
